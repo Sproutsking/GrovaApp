263808ddf8bb8740f86a85fd677565d622c18ccb
Emmanuel Ali-sun <137850759+EXTINCTION001@users.noreply.github.com>
2026-06-26 18:42:38 +0000

    Add OneSignal service worker entry files

---
+++ public/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,5 @@
+try {
+  importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+} catch (err) {
+  importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKUpdaterWorker.js");
+}
